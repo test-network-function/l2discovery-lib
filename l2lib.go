1f1c859be16be3a9939e232a0b6256601f0b0eb9
David Elie-Dit-Cosaque <deliedit@redhat.com>
2023-03-29 16:30:25 -0500

    Fixed for missed interfaces in interface list

--- l2lib.go
+++ l2lib.go
@@ -50,7 +50,7 @@ const (
 	L2DiscoveryContainerName       = "l2discovery"
 	timeoutDaemon                  = time.Second * 60
 	L2DiscoveryDuration            = time.Second * 15
-	l2DiscoveryImage               = "quay.io/testnetworkfunction/l2discovery:v8"
+	l2DiscoveryImage               = "quay.io/testnetworkfunction/l2discovery:v9"
 )
 
 type L2DaemonsetMode int64
